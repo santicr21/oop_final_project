0ee02ad4e3f238ca18160345925191a490de3192
Santiago Caicedo Rojas <santiago.caicedo@globant.com>
2023-08-22 12:27:23 -0500

    Added: University class with its attributes, getters and setters

--- src/model/University.java
+++ src/model/University.java
@@ -1,2 +1,33 @@
-package model;public class University {
+package model;
+
+import java.util.ArrayList;
+
+public class University {
+    private ArrayList <Professor> teachers;
+    private ArrayList <Student> students;
+    private ArrayList <Class> classes;
+
+    public ArrayList<Professor> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(ArrayList<Professor> teachers) {
+        this.teachers = teachers;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public ArrayList<Class> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(ArrayList<Class> classes) {
+        this.classes = classes;
+    }
 }
