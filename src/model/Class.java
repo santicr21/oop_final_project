e463766ea97aadef01112b68ad8b09e685488f31
Santiago Caicedo Rojas <santiago.caicedo@globant.com>
2023-08-22 12:23:35 -0500

    Added: Students class class

---
+++ src/model/Class.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Class {
+    private int id;
+    private String name;
+    private ArrayList<Student> students;
+    private Professor teacher;
+    private String assignedClassroom;
+
+}
