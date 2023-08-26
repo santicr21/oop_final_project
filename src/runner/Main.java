af81172312872c233419bed1eedea8c1617cadcf
Santiago Caicedo Rojas <santiago.caicedo@globant.com>
2023-08-26 09:19:47 -0500

    Added: New functions to search classes and students

--- src/runner/Main.java
+++ src/runner/Main.java
@@ -4,39 +4,11 @@ import model.University;
 import model.professors.FullTimeProfessor;
 import model.professors.PartTimeProfessor;
 import model.Class;
-
-import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    int classesId = 0, professorsId = 0, studentsId = 0;
-
-    // External functions
-    public static Class classExists(int classId, ArrayList <Class> allClasses) {
-        Class classMatched = null;
-
-        for (Class cls: allClasses) {
-            if (cls.getId() == classId){
-                classMatched = cls;
-            }
-        }
-
-        return classMatched;
-    }
-
-    public static Student studentExists(int studentId, ArrayList <Student> allStudents) {
-        Student studentMatched = null;
-
-        for (Student student: allStudents) {
-            if (student.getId() == studentId){
-                studentMatched = student;
-            }
-        }
-
-        return studentMatched;
-    }
 
     public static int catchException (Scanner scan) {
         int number = -1;
@@ -59,12 +31,12 @@ public class Main {
     }
 
     // Print classes indicating its id.
-    public static void printClasses(ArrayList <Class> allClasses) {
+    public static void printClasses(University university) {
         int classIdOption = 0;
         Scanner scan = new Scanner(System.in);
 
         // Printing all classes id's
-        for (Class cls: allClasses) {
+        for (Class cls: university.getClasses()) {
             System.out.println("Class with id: " + cls.getId());
         }
 
@@ -76,7 +48,7 @@ public class Main {
                 break;
 
             // Printing all classes with its attributes
-            Class cls = classExists(classIdOption, allClasses);
+            Class cls = university.classExists(classIdOption);
 
             if (cls != null) {
                 System.out.println("Class id: " + cls.getId());
@@ -92,7 +64,7 @@ public class Main {
         }
     }
 
-    static void createStudent(ArrayList<Student> allStudents, ArrayList<Class> allClasses, Main m) {
+    static void createStudent(University university) {
         String newStudentName;
         int newStudentAge, classId;
         Scanner scan = new Scanner(System.in);
@@ -112,11 +84,13 @@ public class Main {
             return;
 
         // Adding the new student to the db
-        Student newStudent = new Student(m.studentsId++, newStudentName, newStudentAge);
-        allStudents.add(newStudent);
+        int studentsId = university.getStudentsId();
+        Student newStudent = new Student(studentsId, newStudentName, newStudentAge);
+        university.setStudentsId(studentsId + 1);
+        university.getStudents().add(newStudent);
 
         // Getting the class that matches its id with the requested to add the student to that class
-        Class cls = classExists(classId, allClasses);
+        Class cls = university.classExists(classId);
 
         if(cls != null) {
             ArrayList <Student> currentClassStudents = cls.getStudents();
@@ -126,11 +100,11 @@ public class Main {
         }
         else {
             System.out.println("That class with that id does not exists, try again!");
-            m.studentsId--;
+            // studentsId--;
         }
     }
 
-    public static void createClass(ArrayList <Student> allStudents, ArrayList <Professor> allProfessors, ArrayList <Class> allClasses, Main m) {
+    public static void createClass(University university) {
         Scanner scan = new Scanner(System.in); // Scanner
         String newClassName; // The name of the new class
         int professorId; // The professor id that will be teaching in that class
@@ -146,13 +120,14 @@ public class Main {
         // Ask for the number of students to add in the class
         System.out.println("Write the number of students that will be in that class, make sure that there are enough students! and at least 1 ");
         numberOfStudents = catchException(scan);
-        if (numberOfStudents == -1)
+
+        if (numberOfStudents == -1) // Verify if input is a valid number
             return;
-        else if (numberOfStudents > allStudents.size()) {
+        else if (numberOfStudents > university.getStudents().size()) { // verify if number of students to be added entered is valid
             System.out.println("There are more students than registered, try again");
             return;
         }
-        else if(numberOfStudents <= 0) {
+        else if(numberOfStudents <= 0) { // Verify if number of students to be added is not negative
             System.out.println("The integer number must be greater or equal than 1!");
             return;
         }
@@ -165,7 +140,7 @@ public class Main {
                 return;
 
             // Search for the student who matches with the requested id
-            Student student = studentExists(studentId, allStudents);
+            Student student = university.studentExists(studentId);
             if (student != null){
                 classStudents.add(student);
             }
@@ -176,7 +151,6 @@ public class Main {
         }
 
         scan.nextLine();
-
         System.out.println("Write the assigned classroom id: ");
         String classroomId = scan.nextLine();
 
@@ -186,17 +160,14 @@ public class Main {
         if (professorId == -1)
             return;
 
-        // Search for the professor who matches the requested id
-        for (Professor professor: allProfessors) {
-            if (professor.getProfessorId() == professorId) {
-                professorExists = true;
-                // Create the new class and add it to the db
-                Class newClass = new Class(m.classesId++, newClassName, classStudents, professor, classroomId);
-                allClasses.add(newClass);
-            }
+        Professor professor = university.professorExists(professorId); // Search the professor
+        if (professor != null) {
+            int classesId = university.getClassesId();
+            Class newClass = new Class(classesId, newClassName, classStudents, professor, classroomId);
+            university.setClassesId(classesId + 1);
+            university.getClasses().add(newClass);
         }
-
-        if (!professorExists) {
+        else {
             System.out.println("Professor does not exist, try again!");
             return;
         }
@@ -204,7 +175,7 @@ public class Main {
         System.out.println("Class registered!");
     }
 
-    public static void listStudentsClassesById (ArrayList <Class> allClasses) {
+    public static void listStudentsClassesById (University university) {
         Scanner scan = new Scanner(System.in);
         int studentId;
         ArrayList <Student> studentsInCurrentClass;
@@ -216,12 +187,10 @@ public class Main {
             return;
 
         // Iterate over all classes to find the student
-
-        for (Class cls: allClasses) {
+        for (Class cls: university.getClasses()) {
             studentsInCurrentClass = cls.getStudents();
 
-            // Iterate the students of that class to see if any of these match with the requested student id.
-            Student student = studentExists(studentId, studentsInCurrentClass);
+            Student student = cls.studentInClass(studentId); // Search for the student given the student id
             if (student != null) {
                 System.out.println("Class id: " + cls.getId());
                 System.out.println("Class name: " + cls.getName());
@@ -235,71 +204,7 @@ public class Main {
     }
 
     public static void main(String args[]) {
-        //Classes, professors and students id
-        Main m = new Main();
 
-        // All university (professors, students and classes)
-        University university = new University();
-        university.setTeachers(new ArrayList <Professor> ());
-        university.setStudents(new ArrayList <Student> ());
-        university.setClasses(new ArrayList <Class> ());
-        ArrayList <Professor> allProfessors = university.getTeachers();
-        ArrayList <Student> allStudents = university.getStudents();
-        ArrayList <Class> allClasses = university.getClasses();
-
-        // Initializing professors (time-part and full time)
-        PartTimeProfessor ptp1 = new PartTimeProfessor("Felipe", 2000000, 12, m.professorsId++), ptp2 = new PartTimeProfessor("Orlando", 1500000, 23, m.professorsId++);
-        FullTimeProfessor ftp1 = new FullTimeProfessor("Roberto", 1000000, 30, m.professorsId++), ftp2 = new FullTimeProfessor("Oscar", 3500000, 12, m.professorsId++);
-
-        // Adding professors to the db
-        allProfessors.add(ftp1);
-        allProfessors.add(ftp2);
-        allProfessors.add(ptp1);
-        allProfessors.add(ptp2);
-
-        // Creating new students
-        Student s1 = new Student(m.studentsId++, "Pedro", 18), s2 = new Student(m.studentsId++, "Daniel", 20), s3 = new Student(m.studentsId++, "Juan", 19);
-        Student s4 = new Student(m.studentsId++, "Juana", 18), s5 = new Student(m.studentsId++, "Rodrigo", 20), s6 = new Student(m.studentsId++, "Camila", 19);
-
-        // Adding students to the university
-        allStudents.add(s1);
-        allStudents.add(s2);
-        allStudents.add(s3);
-        allStudents.add(s4);
-        allStudents.add(s5);
-        allStudents.add(s6);
-
-        ArrayList <Student> studentsInClass1 = new ArrayList<>();
-        studentsInClass1.add(s1);
-        studentsInClass1.add(s2);
-        studentsInClass1.add(s3);
-
-        ArrayList <Student> studentsInClass2 = new ArrayList<>();
-        studentsInClass2.add(s1);
-        studentsInClass2.add(s2);
-        studentsInClass2.add(s3);
-
-        ArrayList <Student> studentsInClass3 = new ArrayList<>();
-        studentsInClass3.add(s1);
-        studentsInClass3.add(s2);
-        studentsInClass3.add(s3);
-
-        ArrayList <Student> studentsInClass4 = new ArrayList<>();
-        studentsInClass4.add(s1);
-        studentsInClass4.add(s2);
-        studentsInClass4.add(s3);
-
-        // Creating classes where all students registered are in all classes
-        Class c1 = new Class(m.classesId++, "Spanish", studentsInClass1, ptp1, "classroom01");
-        Class c2 = new Class(m.classesId++, "English", studentsInClass2, ptp2, "classroom02");
-        Class c3 = new Class(m.classesId++, "Introduction to programming", studentsInClass3, ftp1, "classroom03");
-        Class c4 = new Class(m.classesId++, "Introduction to system modeling", studentsInClass4, ptp1, "classroom04");
-
-        //Adding classes to the university
-        allClasses.add(c1);
-        allClasses.add(c2);
-        allClasses.add(c3);
-        allClasses.add(c4);
 
         System.out.println("Welcome to the University tracking system!, type the option you want to use:");
         int option = 1;
@@ -323,16 +228,16 @@ public class Main {
                     printProfessorsData (allProfessors);
                     break;
                 case 2:
-                    printClasses (allClasses);
+                    printClasses (university);
                     break;
                 case 3:
-                    createStudent (allStudents, allClasses, m);
+                    createStudent (university);
                     break;
                 case 4:
-                    createClass (allStudents, allProfessors, allClasses, m);
+                    createClass (university);
                     break;
                 case 5:
-                    listStudentsClassesById (allClasses);
+                    listStudentsClassesById (university);
                     break;
                 case 6:
                     System.out.println("Good bye :)!");
